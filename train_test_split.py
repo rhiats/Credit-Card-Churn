0e2267037c633a048308910c7fe59be2fb1c3f05
Rhia Singh <rhia.singh22@gmail.com>
2025-12-01 11:11:27 -0500

    Update dataset

--- train_test_split.py
+++ train_test_split.py
@@ -1,7 +1,21 @@
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
-# X = features, y = labels (if supervised)
+
+df = pd.read_csv("credit_card_churn.csv")
+
+X = df.iloc[:, :20]
+y = df["Attrition_Flag"]
+
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, shuffle=True, random_state=42
 )
+
+X_train.to_pickle("X_train.pkl")
+X_test.to_pickle("X_test.pkl")
+y_train.to_pickle("y_train.pkl")
+y_test.to_pickle("y_test.pkl")
+
+
+
 
