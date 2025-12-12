57dac3ece83bb2abca524e1919d7891047d7444a
Rhia Singh <rhia.singh22@gmail.com>
2025-12-12 17:15:29 -0500

    Search grid for best random forest parameters

--- random_forest.py
+++ random_forest.py
@@ -9,6 +9,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score
+from sklearn.model_selection import RandomizedSearchCV
 
 
 df = pd.read_pickle("X_train.pkl")
@@ -37,9 +38,9 @@ X_train['TransCt_x_Inactive'] = X_train['Total_Trans_Ct'] * X_train['Months_Inac
 
 X_train['Low_Activity'] = (X_train['Total_Trans_Ct'] < X_train['Total_Trans_Ct'].median()).astype(int) #Low activity based on credit card activity
 
-X_train['High_Credit_User'] = (X_train['Credit_Limit'] > X_train['Credit_Limit'].quantile(0.75)).astype(int) #Low activity based on credit card activity
+#X_train['High_Credit_User'] = (X_train['Credit_Limit'] > X_train['Credit_Limit'].quantile(0.75)).astype(int) #Low activity based on credit card activity
 
-X_train['Dormancy_Score'] = X_train['Months_Inactive_12_mon'] / 12
+#X_train['Dormancy_Score'] = X_train['Months_Inactive_12_mon'] / 12
 
 
 
@@ -60,11 +61,43 @@ df_v['TransCt_x_Inactive'] = df_v['Total_Trans_Ct'] * df_v['Months_Inactive_12_m
 
 df_v['Low_Activity'] = (df_v['Total_Trans_Ct'] < df_v['Total_Trans_Ct'].median()).astype(int)
 
-df_v['High_Credit_User'] = (df_v['Credit_Limit'] > df_v['Credit_Limit'].quantile(0.75)).astype(int)
+#df_v['High_Credit_User'] = (df_v['Credit_Limit'] > df_v['Credit_Limit'].quantile(0.75)).astype(int)
 
-df_v['Dormancy_Score'] = df_v['Months_Inactive_12_mon'] / 12
+#df_v['Dormancy_Score'] = df_v['Months_Inactive_12_mon'] / 12
 
 
+
+#Search Grid
+
+rf = RandomForestClassifier(random_state=42)
+
+param_dist = {
+    'n_estimators': [100, 200, 400, 600],
+    'max_depth': [5, 10, 15, 20, None],
+    'min_samples_split': [2, 5, 10, 20],
+    'min_samples_leaf': [1, 2, 4, 8],
+    'max_features': ['sqrt', 'log2', 0.5, None],
+    'bootstrap': [True, False]
+}
+
+rs = RandomizedSearchCV(
+    rf, param_dist, n_iter=30,
+    scoring='f1', cv=5, verbose=2, n_jobs=-1
+)
+rs.fit(X_train, y_train)
+
+best_rf = rs.best_estimator_
+
+y_pred = best_rf.predict(df_v)
+
+score = f1_score(y_valid, y_pred)
+
+print("F1 Score: {}".format(score)) 
+
+print("Best RF:",best_rf)
+
+
+"""
 classifier = RandomForestClassifier(n_estimators=100, random_state=42)
 classifier.fit(X_train, y_train)
 
@@ -73,6 +106,9 @@ y_pred = classifier.predict(df_v)
 score = f1_score(y_valid, y_pred)
 
 print("F1 Score: {}".format(score)) 
+"""
+
+
 
 #F1 Score Iteration 1: 0.7452054794520548
 #F1 Score Interation 2: 0.7493112947658402 (Add Avg_Trans_Amt)
