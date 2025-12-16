70478f608660c51e1b06c4bb4f103007d97eee9a
Rhia Singh <rhia.singh22@gmail.com>
2025-12-16 15:51:48 -0500

    Randomly sample majority class

--- random_sample.py
+++ random_sample.py
@@ -5,3 +5,19 @@ import pandas as pd
 X = pd.read_pickle("X_train.pkl")
 y = pd.read_pickle("y_train.pkl")
 
+X["y"] = y
+
+existing_customer_num = X["y"].value_counts()["Existing Customer"]
+attrited_customer_num = X["y"].value_counts()["Attrited Customer"]
+
+print("Number of Existing Customers are: {}".format(existing_customer_num))
+print("Number of Attrited Customers are: {}".format(attrited_customer_num))
+
+#Select random sample of majority class(868)
+df_sample_maj_class = X[X['y']=="Existing Customer"].sample(n=attrited_customer_num, random_state=1)
+
+df = pd.concat([df_sample_maj_class,X[X['y']=="Attrited Customer"]],ignore_index=True)
+
+df.to_pickle("training_random_sample.pkl")
+
+
